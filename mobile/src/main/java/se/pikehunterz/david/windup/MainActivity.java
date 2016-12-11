a7c95808fca660b4c6f02d4cbd2dc6438625c2e2
davidbergvik <davidbergvik@gmail.com>
2016-12-11 02:49:08 +0100

    working on setting angle for wind direction

--- mobile/src/main/java/se/pikehunterz/david/windup/MainActivity.java
+++ mobile/src/main/java/se/pikehunterz/david/windup/MainActivity.java
@@ -11,16 +11,19 @@ import android.widget.RelativeLayout;
 
 import com.google.android.gms.location.LocationResult;
 
+import java.io.Console;
+import java.util.Random;
+
 public class MainActivity extends Activity {
     RelativeLayout rl_arrow;
     ImageView iv_circle;
-    boolean isLoading = true;
-
+    boolean isLoading;
+    int currentWindAngle;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        isLoading = true;
         rl_arrow = (RelativeLayout) findViewById(R.id.rl_arrow);
         iv_circle = (ImageView) findViewById(R.id.circle);
         iv_circle.setOnClickListener(new View.OnClickListener() {
@@ -29,27 +32,29 @@ public class MainActivity extends Activity {
             public void onClick(View v) {
 
                 if (isLoading) {
-                    stopSpinning();
                     isLoading = false;
+                    loadPosition();
+
                 } else {
+                    stopSpinning();
+                    /*
                     startSpinning();
                     isLoading = true;
+                    */
                 }
 
             }
 
         });
-        loadPosition();
-    }
-
-    private void loadPosition() {
         startSpinning();
-
+    }
+    private void loadPosition() {
+        //Temp function for calculating curent wind angle
+        int angle = 0;
      /*   LocationResult locResult = fetchPosition();
         showWindData(locResult);
-        /* When done:
-        stopSpinning();
-*/
+     */
+        currentWindAngle = getMedianWindAngle(angle);
     }
     /*
     //Get current postition of device from gps
@@ -62,16 +67,64 @@ public class MainActivity extends Activity {
 
     }
     */
+    private int getMedianWindAngle(int windData){
+        int medianAngle;
+        if (windData == 0) {
+
+            Random rand = new Random();
+            windData = rand.nextInt(360);
+            medianAngle = windData;
+        }else{
+            medianAngle= 45;
+        }
+
+        return medianAngle;
+    }
+
     private void stopSpinning() {
         rl_arrow.clearAnimation();
     }
 
     public void startSpinning() {
-        Animation loadingSpin = null;
-        RotateAnimation rotate = new RotateAnimation(0,360, Animation.RELATIVE_TO_SELF,0.5f , Animation.RELATIVE_TO_SELF,0.5f );
-        rotate.setDuration(2000);
-        rotate.setRepeatCount(Animation.INFINITE);
-        rl_arrow.startAnimation(rotate);
+        RotateAnimation ani_rotate = new RotateAnimation(0,360, Animation.RELATIVE_TO_SELF,0.5f , Animation.RELATIVE_TO_SELF,0.5f );
+        ani_rotate .setDuration(2000);
+        ani_rotate .setRepeatCount(Animation.INFINITE);
+        ani_rotate .setAnimationListener(new Animation.AnimationListener() {
+            @Override
+            public void onAnimationStart(Animation animation) {}
+
+            @Override
+            public void onAnimationRepeat(Animation animation) {}
+
+            @Override
+            public void onAnimationEnd(Animation animation) {
+                if(!isLoading){
+                    stopSpinning();
+                    animateToCurrentWindAngle(currentWindAngle);
+                }
+            }
+        });
+
+        rl_arrow.startAnimation(ani_rotate );
+
     }
+    private void animateToCurrentWindAngle(final int direction){
+        RotateAnimation ani_setWindRotation = new RotateAnimation(0,direction, Animation.RELATIVE_TO_SELF,0.5f , Animation.RELATIVE_TO_SELF,0.5f );
+        ani_setWindRotation.setDuration(700);
+        ani_setWindRotation.setAnimationListener(new Animation.AnimationListener() {
+            @Override
+            public void onAnimationStart(Animation animation) {}
+
+            @Override
+            public void onAnimationRepeat(Animation animation) {}
+
+            @Override
+            public void onAnimationEnd(Animation animation) {
+                rl_arrow.setRotation(currentWindAngle);
+            }
+        });
+        rl_arrow.startAnimation(ani_setWindRotation );
+    }
+
 
 }
