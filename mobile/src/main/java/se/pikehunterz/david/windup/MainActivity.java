bf64d5c6802eb7f69efc8d43eebce3bfb70b2bc4
jespersjberg <cc._3@hotmail.com>
2016-12-15 20:15:22 +0100

    First commit GPS-position

--- mobile/src/main/java/se/pikehunterz/david/windup/MainActivity.java
+++ mobile/src/main/java/se/pikehunterz/david/windup/MainActivity.java
@@ -1,14 +1,23 @@
 package se.pikehunterz.david.windup;
 
+import android.Manifest;
 import android.app.Activity;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.os.Build;
 import android.os.Bundle;
 
+import android.provider.Settings;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
-
 import retrofit.Callback;
 import retrofit.Response;
 import retrofit.Retrofit;
@@ -25,6 +34,7 @@ import android.widget.RelativeLayout;
 
 
 
+
 public class MainActivity extends Activity {
     RelativeLayout rl_arrow;
     ImageView iv_circle;
@@ -34,15 +44,24 @@ public class MainActivity extends Activity {
     public String windString;
     public String windDirection;
 
+
+    private Button b;
+    private TextView t;
+    private LocationManager locationManager;
+    private LocationListener listener;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Button b1;
-        final TextView tv, tv1;
+        final Button b1, b2;
+        final TextView tv, tv1, tv2;
         b1 = (Button) findViewById(R.id.button);
+        b2 = (Button) findViewById(R.id.buttonGPS);
         tv = (TextView) findViewById(R.id.textViewTest);
         tv1 = (TextView) findViewById(R.id.textView);
+        tv2 = (TextView) findViewById(R.id.coordinates);
         rl_arrow = (RelativeLayout) findViewById(R.id.rl_arrow);
         iv_circle = (ImageView) findViewById(R.id.circle);
         b1.setOnClickListener(new View.OnClickListener() {
@@ -56,17 +75,17 @@ public class MainActivity extends Activity {
 
                         //Toast.makeText(new MainActivity(), "Your Message", Toast.LENGTH_LONG).show();
 
-                        for(int i = 0; i < responseData.getParameters().size(); i++ ){
+                        for (int i = 0; i < responseData.getParameters().size(); i++) {
                             Parameter data = responseData.getParameters().get(i);
                             windString = "mafakka" + data.getName();
-                            if(data.getName().equals("gust")){
-                                if(data.getValues().get(0) != null){
+                            if (data.getName().equals("gust")) {
+                                if (data.getValues().get(0) != null) {
                                     currentWind = data.getValues().get(0);
                                     tv.setText(currentWind.toString());
                                 }
                             }
-                            if(data.getName().equals("wd")){
-                                if(data.getValues().get(0) != null){
+                            if (data.getName().equals("wd")) {
+                                if (data.getValues().get(0) != null) {
                                     windDirection = data.getValues().get(0).toString();
                                     tv1.setText(windDirection);
                                 }
@@ -80,10 +99,79 @@ public class MainActivity extends Activity {
 
                     @Override
                     public void onFailure(Throwable t) {
-                        Log.e("Failed",t.getMessage());
+                        Log.e("Failed", t.getMessage());
                     }
                 });
 
+
+
+        }
+
+        });
+
+        setContentView(R.layout.activity_main);
+
+        t = (TextView) findViewById(R.id.coordinates);
+        b = (Button) findViewById(R.id.buttonGPS);
+
+        locationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
+
+
+        listener = new LocationListener() {
+            @Override
+            public void onLocationChanged(Location location) {
+                t.append("\n " + location.getLongitude() + " " + location.getLatitude());
+                Toast.makeText(getApplicationContext(), String.valueOf(location.getLatitude()), Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onStatusChanged(String s, int i, Bundle bundle) {
+
+            }
+
+            @Override
+            public void onProviderEnabled(String s) {
+
+            }
+
+            @Override
+            public void onProviderDisabled(String s) {
+
+                Intent i = new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS);
+                startActivity(i);
+            }
+        };
+
+        configure_button();
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        switch (requestCode){
+            case 10:
+                configure_button();
+                break;
+            default:
+                break;
+        }
+    }
+
+    void configure_button(){
+        // first check for permissions
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                requestPermissions(new String[]{Manifest.permission.ACCESS_COARSE_LOCATION,Manifest.permission.ACCESS_FINE_LOCATION,Manifest.permission.INTERNET}
+                        ,10);
+            }
+            return;
+        }
+        // this code won't execute IF permissions are not allowed, because in the line above there is return statement.
+        b.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                //noinspection MissingPermission
+                locationManager.requestLocationUpdates("gps", 5, 0, listener);
             }
         });
 
@@ -103,8 +191,11 @@ public class MainActivity extends Activity {
             }
 
         });
+
         loadPosition();
     }
+
+
 
     private void loadPosition() {
         startSpinning();
